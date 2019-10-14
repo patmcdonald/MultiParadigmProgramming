37717f018701a73a9027c1765822c12c7ef723ab
Pat McDonald <patmcdonald@me.com>
2019-10-14 13:38:34 +0100

    end of part one!

--- shop.c
+++ shop.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Customer {
-  char* name;
-  double budget;
-};
-
 struct Product {
   char* name;
   double price;
 };
 
+struct ProductStock {
+  struct Product product;
+  int quantity;
+};
+
+struct Shop
+{
+double cash;
+  struct ProductStock stock[20];
+};
+
+struct Customer 
+{
+  char* name;
+  double budget;
+  struct ProductStock shoppingList[10];
+};
+
+void printProduct(struct Product p) 
+{
+  printf("-------------------\n");
+  printf("PRODUCT NAME: %s \nPRODUCT PRICE: %.2f\n", p.name, p.price);
+  printf("-------------------\n");
+}
+
+void printCustomer(struct Customer c)
+{
+   printf("-------------------\n");
+  printf("CUSTOMER NAME: %s \nCUSTOMER BUDGET: %.2f\n", c.name, c.budget);
+  printf("-------------------\n");
+}
 
 int main(void)
 {
   struct Customer pat = {"Pat", 100.0};
-  printf("Customer name is  %s\n", pat.name); 
+  printCustomer(pat);
   
   struct Product coke = {"Can Coke", 1.10};
-  printf("The %s costs %2f\n", coke.name, coke.price);
+  printProduct(coke);
   
+  struct ProductStock cokeStock = {coke, 20 };
+  printf("The shop has %d of the product %s\n", cokeStock.quantity, cokeStock.product.name);
   return 0;
 };
