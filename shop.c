e08851a6335011ff53f856f4296d10afd385513d
Pat McDonald <patmcdonald@me.com>
2019-10-19 08:56:26 +0100

    code at 8 mins in vid

--- shop.c
+++ shop.c
@@ -1,3 +1,4 @@
+//standard C packages
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -26,6 +27,7 @@ struct Customer
   char* name;
   double budget;
   struct ProductStock shoppingList[10];
+  int index;
 };
 
 void printProduct(struct Product p) 
@@ -40,17 +42,25 @@ void printCustomer(struct Customer c)
    printf("-------------------\n");
   printf("CUSTOMER NAME: %s \nCUSTOMER BUDGET: %.2f\n", c.name, c.budget);
   printf("-------------------\n");
+  for(int i = 0; i < c.index; i++)
+  {
+    printProduct(c.shoppingList[i].product);
+    printf("%s ORDERS %d OF ABOVE PRODUCT", c.name, c.shoppingList[i].quantity);
+  }
 }
 
+//Main method
 int main(void)
 {
   struct Customer pat = {"Pat", 100.0};
-  printCustomer(pat);
+  
   
   struct Product coke = {"Can Coke", 1.10};
-  printProduct(coke);
+  // printProduct(coke);
   
   struct ProductStock cokeStock = {coke, 20 };
+  pat.shoppingList[pat.index++] = cokeStock;
+  printCustomer(pat);
   // printf("The shop has %d of the product %s\n", cokeStock.quantity, cokeStock.product.name);
   // return 0;
 };
