5f9f2ea9ed7475f1c0dff077ddff7a7a8ba676a3
Pat McDonald <patmcdonald@me.com>
2019-10-24 21:27:10 +0100

    final code part2

--- shop.c
+++ shop.c
@@ -66,28 +66,41 @@ struct Shop createAndStockShop()
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
-    while ((read = getline(&line, &len, fp)) != -1) {
+    while ((read = getline(&line, &len, fp)) != -1) 
+    {
         //printf("Retrieved line of length %zu:\n", read);
         //printf("%s IS A LINE", line);
-        char *name = strtok(line, ",");
+        char *n = strtok(line, ",");
         char *p = strtok(NULL, ",");
         char *q = strtok(NULL, ",");
         int quantity = atoi(q);
         double price = atof(p);
+        char *name = malloc(sizeof(char) * 50);//memory allocation - set to 50
+        strcpy(name, n);//string copy to dynamically allocated memory
         struct Product product = {name, price};
         struct ProductStock stockItem = {product, quantity};
         shop.stock[shop.index ++] = stockItem;
-        printf("NAME OF PRODUCT %s PRICE %.2f QUANTITY %d \n", name, price, quantity);
+       // printf("NAME OF PRODUCT %s PRICE %.2f QUANTITY %d \n", name, price, quantity);
     }
+  
 
     return shop;
-
+}
+    void printShop(struct Shop s)
+    {
+      printf("Shop has %.2f in cash\n", s.cash);
+        for (int i = 0; i < s.index; i++)
+        {
+          printProduct(s.stock[i].product);
+          printf("The shop has %d of the above\n", s.stock[i].quantity);
+        }
+    }
     // fclose(fp);
     // if (line)
     //     free(line);
     // exit(EXIT_SUCCESS);
   
-}
+
 
 //Main method
 int main(void)
@@ -107,11 +120,9 @@ int main(void)
 
 //   printCustomer(pat);
 
-  createAndStockShop();
-  // printf("The shop has %d of the product %s\n", cokeStock.quantity, cokeStock.product.name);
+struct Shop shop = createAndStockShop();
+printShop(shop);
+//printf("The shop has %d of the product %s\n", cokeStock.quantity, cokeStock.product.name);
  return 0;
 
-
-};
-
-
+}
