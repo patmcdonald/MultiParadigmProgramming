db8599c5753b4a0e636ea659520ae347f5b18a38
Pat McDonald <patmcdonald@me.com>
2019-11-11 12:07:26 +0000

    update csv files, printCustomer()- line 126

--- shop.c
+++ shop.c
@@ -39,6 +39,91 @@ void printProduct(struct Product p)
   printf("-------------------\n");
 }
 
+//Stock shop method: read in stock.csv
+struct Shop createAndStockShop()
+{
+  FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen("stock.csv", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+        //read cash from csv
+        getline(&line, &len, fp);
+        double shopCash = atof(line);
+        struct Shop shop = {shopCash};
+        
+
+    while ((read = getline(&line, &len, fp)) != -1) 
+    {
+        char *n = strtok(line, ",");
+        char *p = strtok(NULL, ",");
+        char *q = strtok(NULL, ",");
+        // datatype conversions
+        int quantity = atoi(q);
+        double price = atof(p);
+        char *name = malloc(sizeof(char) * 50);//memory allocation - set to 50
+        strcpy(name, n);//string copy to dynamically allocated memory
+        
+        struct Product product = {name, price};
+        struct ProductStock stockItem = {product, quantity};
+        shop.stock[shop.index++] = stockItem;
+    }
+        return shop;
+}
+//printing shop data
+void printShop(struct Shop s)
+    {
+      printf("Shop has %.2f in cash\n", s.cash);
+        for (int i = 0; i < s.index; i++)
+        {
+          printProduct(s.stock[i].product);
+          printf("The shop has %d of the above\n", s.stock[i].quantity);
+        }
+    }
+
+//read customer order from order.csv
+struct Customer custOrder()
+{
+	FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int i = 0;
+
+    fp = fopen("order.csv", "r");
+    //exit program if file not present
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+	
+  //read customer name and budget
+	read = getline(&line, &len, fp);
+    char *a = strtok(line, ",");
+    char *b = strtok(NULL, ",");
+    char *custName = malloc(sizeof(char) * 50);
+    double custBudget = atof(b);
+    strcpy(custName, a); 
+	struct Customer customer = { custName, custBudget};
+    printf("CUSTOMER NAME: %s \nBUDGET: %.2f\n", custName, custBudget);
+    while ((read = getline(&line, &len, fp)) != -1) {
+		// TODO process remaining lines
+    char *p = strtok(line, ",");
+			char *q = strtok(NULL, ",");
+			int quantity = atoi(q);
+			char *productname = malloc(sizeof(char)*50);
+			strcpy(productname, p);
+			struct Product product = {productname};
+			struct ProductStock orderItem = {product, quantity};
+			customer.shoppingList[customer.index++]=orderItem;
+
+	}        
+	return customer;
+}   
+
+//customer and order data
 void printCustomer(struct Customer c)
 {
    printf("-------------------\n");
@@ -54,53 +139,6 @@ void printCustomer(struct Customer c)
   }
 }
 
-//Stock method
-struct Shop createAndStockShop()
-{
-  struct Shop shop = { 200 };
-  FILE * fp;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    fp = fopen("stock.csv", "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
-
-    while ((read = getline(&line, &len, fp)) != -1) 
-    {
-        //printf("Retrieved line of length %zu:\n", read);
-        //printf("%s IS A LINE", line);
-        char *n = strtok(line, ",");
-        char *p = strtok(NULL, ",");
-        char *q = strtok(NULL, ",");
-        int quantity = atoi(q);
-        double price = atof(p);
-        char *name = malloc(sizeof(char) * 50);//memory allocation - set to 50
-        strcpy(name, n);//string copy to dynamically allocated memory
-        struct Product product = {name, price};
-        struct ProductStock stockItem = {product, quantity};
-        shop.stock[shop.index ++] = stockItem;
-       // printf("NAME OF PRODUCT %s PRICE %.2f QUANTITY %d \n", name, price, quantity);
-    }
-  
-
-    return shop;
-}
-    void printShop(struct Shop s)
-    {
-      printf("Shop has %.2f in cash\n", s.cash);
-        for (int i = 0; i < s.index; i++)
-        {
-          printProduct(s.stock[i].product);
-          printf("The shop has %d of the above\n", s.stock[i].quantity);
-        }
-    }
-    // fclose(fp);
-    // if (line)
-    //     free(line);
-    // exit(EXIT_SUCCESS);
-  
 
 
 //Main method
